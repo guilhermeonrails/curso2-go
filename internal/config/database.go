f4c7a66395fffae707a51d42d3e88b6eb83df095
guilhermelima <guilhermelimaruby@gmail.com>
2025-04-16 16:38:47 -0300

    aula-1.2

---
+++ internal/config/database.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"log"
+	"myapi/internal/models"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	// Conexão com o Postgres (usando host "db" pois o docker-compose cria essa rede)
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Erro ao conectar com o BD: %v", err)
+	}
+	DB = db
+
+	DB.AutoMigrate(&models.Item{})
+}
