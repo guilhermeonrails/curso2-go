082c35902ff7de2553361f814e2c85d86ecf95eb
guilhermelima <guilhermelimaruby@gmail.com>
2025-04-17 15:31:54 -0300

    aula4.1

--- internal/models/item.go
+++ internal/models/item.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type Item struct {
 	Id         uint    `gorm:"primaryKey" json:"id"`
 	Nome       string  `json:"nome"`
@@ -7,4 +9,11 @@ type Item struct {
 	Descricao  string  `json:"descricao"`
 	Preco      float64 `json:"preco"`
 	Quantidade int     `json:"quantidade"`
+}
+
+func (i *Item) Validate() error {
+	if i.Preco < 0 {
+		return errors.New("o preço não pode ser negativo")
+	}
+	return nil
 }
